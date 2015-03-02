82812259c72e71af3fcc96994a96ae78a0696df6
Simon D. Levy <simon.d.levy@gmail.com>
2015-03-01 22:52:40 -0500

    Added baselines; changed getYs to getValues

--- realtime_plot.py
+++ realtime_plot.py
@@ -26,6 +26,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 
+
 class RealtimePlotter(object):
     '''
     Real-time scrolling multi-plot over time.  Your data-acquisition code should run on its own thread,
@@ -51,12 +52,11 @@ class RealtimePlotter(object):
         Initializes a multi-plot with specified Y-axis limits as a list of pairs; e.g., 
         [(-1,+1), (0.,5)].  Optional parameters are:
        
-        size          size of display (X axis) in arbitrary time steps
-        window_name   name to display at the top of the figure
-        styles        plot styles (e.g., 'b-', 'r.')
-        ylabels       Y-axis labels
-        yticks        Y-axis tick / grid positions
-        interval_msec animation update in milliseconds
+        size             size of display (X axis) in arbitrary time steps
+        window_name      name to display at the top of the figure
+        styles           plot styles (e.g., 'b-', 'r.'; default='b-')
+        yticks           Y-axis tick / grid positions
+        interval_msec    animation update in milliseconds
         '''
 
         # Row count is provided by Y-axis limits
@@ -68,7 +68,7 @@ class RealtimePlotter(object):
         yticks  = self._check_param(nrows, yticks, 'yticks', [])
 
         # Set up subplots
-        self.fig, axes = plt.subplots(nrows)
+        self.fig, self.axes = plt.subplots(nrows)
         if window_name:
             self.fig.canvas.set_window_title(window_name)
 
@@ -81,24 +81,25 @@ class RealtimePlotter(object):
         y = np.zeros(size)
 
         # Create lines
-        self.lines = [ax.plot(self.x, y, style, animated=True)[0] for ax,style in zip(axes,styles)]
+        self.lines = [ax.plot(self.x, y, style, animated=True)[0] for ax,style in zip(self.axes,styles)]
+
+        # Create baselines, initially hidden
+        self.baselines = [ax.plot(self.x, y, 'k', animated=True)[0] for ax in self.axes]
+        self.baseflags = [False]*nrows
 
         # Add properties as specified
-        [ax.set_ylabel(ylabel) for ax, ylabel in zip(axes, ylabels)]
+        [ax.set_ylabel(ylabel) for ax, ylabel in zip(self.axes, ylabels)]
 
         # Set axis limits
-        [ax.set_xlim((0,size)) for ax in axes]
-        [ax.set_ylim(ylim) for ax,ylim in zip(axes,ylims)]
+        [ax.set_xlim((0,size)) for ax in self.axes]
+        [ax.set_ylim(ylim) for ax,ylim in zip(self.axes,ylims)]
 
         # Set ticks and gridlines
-        [ax.yaxis.set_ticks(ytick) for ax,ytick in zip(axes,yticks)]
-        [ax.yaxis.grid(True if yticks else False) for ax in axes]
-
-        # Set X same for all lines
-        [line.set_xdata(self.x) for line in self.lines]
+        [ax.yaxis.set_ticks(ytick) for ax,ytick in zip(self.axes,yticks)]
+        [ax.yaxis.grid(True if yticks else False) for ax in self.axes]
 
         # XXX Hide X axis ticks and labels for now
-        [ax.xaxis.set_visible(False) for ax in axes]
+        [ax.xaxis.set_visible(False) for ax in self.axes]
 
         # Allow interval specification
         self.interval_msec = interval_msec
@@ -110,20 +111,55 @@ class RealtimePlotter(object):
 
         ani = animation.FuncAnimation(self.fig, self._animate, interval=self.interval_msec, blit=True)
         plt.show()
+  
+    def getValues(self):
+        '''
+        Override this method to return actual Y values at current time.
+        '''
+
+        return None
+
+    def showBaseline(self, axid, value):
+        '''
+        Shows a baseline of specified value for specified row of this multi-plot.
+        '''
+
+        self._axis_check(axid)
+
+        self.baselines[axid].set_ydata(value * np.ones(self.x.shape))
+        self.baseflags[axid] = True
+
+    def hideBaseline(self, axid):
+        '''
+        Hides the baseline for the specified row of this multi-plot.
+        '''
+
+
+        self._axis_check(axid)
+
+        self.baseflags[axid] = False
+
+    def _axis_check(self, axid):
+
+        nrows = len(self.lines)
+
+        if axid < 0 or axid >= nrows:
+
+            raise Exception('Axis index must be in [0,%d)' % nrows)
 
     def _animate(self, t):
 
-        yvals = self.getYs()
+        yvals = self.getValues()
 
         for row, line in enumerate(self.lines, start=1):
             ydata = line.get_ydata()
             ydata = np.roll(ydata, -1)
             ydata[-1] = yvals[row-1]
             line.set_ydata(ydata)
-            
-        return self.lines
 
-# Simpe example with threading
+        return self.lines + [baseline for baseline,flag in zip(self.baselines,self.baseflags) if flag]
+
+# Simple example with threading
 
 class _SinePlotter(RealtimePlotter):
 
@@ -132,13 +168,13 @@ class _SinePlotter(RealtimePlotter):
         RealtimePlotter.__init__(self, [(-1,+1), (-1,+1)], 
                 window_name='Sinewave demo',
                 yticks = [(-1,0,+1),(-1,0,+1)],
-                styles = ['r-', 'b-'], 
+                styles = ['r--', 'b-'], 
                 ylabels=['Slow', 'Fast'])
 
         self.xcurr = 0
 
-    def getYs(self):
-        
+    def getValues(self):
+
         return self._getRow(1), self._getRow(2)
 
     def _getRow(self, row):
