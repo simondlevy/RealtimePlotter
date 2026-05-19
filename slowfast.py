80c3bf14c171feb9086351c49c767043d83383bc
simondlevy <simon.d.levy@gmail.com>
2026-05-18 22:44:54 -0400

    modified:   slowfast.py

--- slowfast.py
+++ slowfast.py
@@ -1,67 +1,47 @@
-#!/usr/bin/env python3
-'''
-Real-time plot demo using sine waves.
+#!/usr/bin/python3
 
-Copyright (C) 2015 Simon D. Levy
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-'''
-
-from realtime_plot import RealtimePlotter
 import numpy as np
+from threading import Thread
+from time import sleep
 
-# Simple example with threading
+from new_realtime_plot import RealtimePlotter
 
-class _SinePlotter(RealtimePlotter):
 
-    def __init__(self):
+def threadfun(plotter):
 
-        RealtimePlotter.__init__(self,
-                [(-1,+1), (-1,+1)], 
+    i = 0
+
+    x = np.linspace(0, 2*np.pi, 1000)
+
+    while True:
+
+        plotter.set_ydata(0, np.sin(x + i / 10.0))
+
+        plotter.set_ydata(1, np.sin(x + i / 2.0))
+
+        i += 1
+
+        sleep(0.02)
+
+
+def main():
+
+    plotter = RealtimePlotter(((-1,+1), (-1,+1)),
                 show_yvals=True,
                 window_name='Sinewave demo',
                 yticks = [(-1,0,+1),(-1,0,+1)],
                 styles = ['r--', 'b-'], 
                 ylabels=['Slow', 'Fast'])
 
-        self.xcurr = 0
 
-    def getValues(self):
-
-        return self._getWave(1), self._getWave(2)
-
-    def _getWave(self, k):
-
-        size = len(self.x)
-        
-        return np.sin(2*k*np.pi*(float(self.xcurr)%size)/size)
-
-
-def _update(plotter):
-
-    from time import sleep
-
-    while True:
-
-        plotter.xcurr += 1
-        sleep(.002)
-
-if __name__ == '__main__':
-
-    import threading
-
-    plotter = _SinePlotter()
-
-    thread = threading.Thread(target=_update, args = (plotter,))
+    thread = Thread(target=threadfun, args=(plotter, ))
     thread.daemon = True
     thread.start()
 
     plotter.start()
- 
+
+
+main()
+
+
+
